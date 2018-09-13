33f4af6524f70a2dd5dd49bd4ab314311cdb6153
abhijithmannath <abhijithmannath@gmail.com>
2018-09-13 14:12:54 +0530

    initial

---
+++ TapUtil/FocusUtil.h
@@ -0,0 +1,13 @@
+//
+//  FocusUtil.h
+//  TapUtil
+//
+//  Created by Saurabh Maurya on 13/09/18.
+//  Copyright © 2018 Tapchief. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FocusUtil : NSObject
++ (void)enable;
+@end
---
+++ TapUtil/TapUtil.h
@@ -0,0 +1,19 @@
+//
+//  TapUtil.h
+//  TapUtil
+//
+//  Created by Saurabh Maurya on 13/09/18.
+//  Copyright © 2018 Tapchief. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TapUtil.
+FOUNDATION_EXPORT double TapUtilVersionNumber;
+
+//! Project version string for TapUtil.
+FOUNDATION_EXPORT const unsigned char TapUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapUtil/PublicHeader.h>
+#import <TapUtil/FocusUtil.h>
+
